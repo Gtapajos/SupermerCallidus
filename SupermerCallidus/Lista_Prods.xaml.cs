324d93e3ce1e2814c40c5efad0f3ef12aeb82b0f
Guilherme Correia Tapajós Araújo <90109601+Gtapajos@users.noreply.github.com>
2022-10-28 16:39:54 -0400

    the full project

---
+++ SupermerCallidus/Lista_Prods.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SupermerCallidus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lista_Prods : ContentPage
+    {
+        MainPage mainPage = new MainPage();
+        public Lista_Prods(List<string> prods)
+        {
+            InitializeComponent();
+            lista.ItemsSource = prods;
+        }
+
+    }
+}
---
+++ SupermerCallidus/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SupermerCallidus
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> Produtos = new List<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            String prod = etComando.Text;
+            Button button = (Button)sender;
+            if (button.Text == "Adicionar Produto")
+            {
+                Produtos.Add(prod);
+            }
+            else if (button.Text == "Remover Produto")
+            {
+                Produtos.Remove(prod);
+            }
+            
+            else if (button.Text == "Ver Lista")
+            {
+                await Navigation.PushAsync(new Lista_Prods(Produtos));
+            }
+
+        }
+
+        public List<string> GetProdutos()
+        {
+            return Produtos;
+        }
+       
+    }
+}
